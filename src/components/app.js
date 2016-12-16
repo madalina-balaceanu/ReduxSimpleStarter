bf0511843e610a31efe01a6998efea77b5a043e1
Madalina Balaceanu <mbalaceanu@axway.com>
2016-12-16 16:24:06 +0200

    add SearchBar

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import SearchBar from '../containers/search_bar'; 
+import BookList from '../containers/book-list';
+import BookDetail from '../containers/book-detail';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <BookList />
+        <BookDetail />
       </div>
     );
   }
